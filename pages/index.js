92f3abce1954426ee54bdd4cf5b5e34624b7982c
Bin Hu <elithrade@gmail.com>
2023-01-30 06:46:31 +0800

    feat/ simple home page content

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 export default function Page() {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a senior software engineer based in Perth, Australia!
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Bin Hu
+          </Heading>
+          <p>
+            Your time is limited, so don&apos;t waste it living someone
+            else&apos;s life.
+          </p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }

2b9be11fc55af2b447a82076cbf9b905f87354fb
Bin Hu <elithrade@gmail.com>
2023-02-14 20:24:22 +0800

    feat/ fix background color typo

--- components/navbar.js
+++ components/navbar.js
@@ -15,15 +15,17 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
+import { NextLink } from 'next/link'
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href
   const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
   return (
     <Link
+      as={NextLink}
       href={href}
       p={2}
-      bg={active ? 'glassTeal' : undefined}
+      bg={active ? 'grassTeal' : undefined}
       color={active ? '#202023' : inactiveColor}
     >
       {children}
